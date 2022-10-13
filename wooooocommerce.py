a0821ece72f6e49e11197c91817beb747f2db7e8
Jim Teunis <jim.teunis@gmail.com>
2022-10-13 14:36:10 +0200

    fix sound bug, run every 5 seconds

--- wooooocommerce.py
+++ wooooocommerce.py
@@ -1,3 +1,4 @@
+from time import sleep
 from woocommerce import API
 import json
 from dateutil import parser
@@ -6,7 +7,7 @@ ds = '2012-03-01T10:00:00Z' # or any date sting of differing formats.
 date = parser.parse(ds)
 from playsound import playsound
 SOUND_FILENAME = 'sound.wav'
-
+sound = None
 
 # change working directory to this folder
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -14,6 +15,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 # check if sound.wav exists
 try:
     open(SOUND_FILENAME, 'r')
+    sound = True
 except IOError:
     print('Audio file not found')
     sound = None
@@ -25,34 +27,37 @@ with open('keys.json') as f:
     consumer_key = keys['consumer_key']
     consumer_secret = keys['consumer_secret']
 
-wcapi = API(
-    url=url,
-    consumer_key=consumer_key,
-    consumer_secret=consumer_secret,
-    version="wc/v3"
-)
+while True:
+    wcapi = API(
+        url=url,
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        version="wc/v3"
+    )
 
-# create file if it doesn't exist
-with open('last_order.txt', 'a+') as f:
-    pass
+    # create file if it doesn't exist
+    with open('last_order.txt', 'a+') as f:
+        pass
 
-latest_order = wcapi.get('orders').json()[0]
-latest_order_str = latest_order['date_created']
-latest_order_time = parser.parse(latest_order['date_created'])
+    latest_order = wcapi.get('orders').json()[0]
+    latest_order_str = latest_order['date_created']
+    latest_order_time = parser.parse(latest_order['date_created'])
 
-# read previous last order
-with open('last_order.txt', 'r') as f:
-    prev_last_order_str = f.read()
-    try:
-        if parser.parse(prev_last_order_str) < latest_order_time:
-            print('new order!!!')
-            if sound:
-                playsound(SOUND_FILENAME)
-            # write new last order
-            with open('last_order.txt', 'w') as f:
-                f.write(latest_order_str)
-        else:
-            print('no new order....')
-            print('yet!')
-    except ValueError:
-        print('no previous order found')
+    # read previous last order
+    with open('last_order.txt', 'r') as f:
+        prev_last_order_str = f.read()
+        try:
+            if parser.parse(prev_last_order_str) < latest_order_time:
+                print('new order!!!')
+                if sound:
+                    playsound(SOUND_FILENAME)
+                # write new last order
+                with open('last_order.txt', 'w') as f:
+                    f.write(latest_order_str)
+            else:
+                print('no new order....')
+                print('yet!')
+        except ValueError:
+            print('no previous order found')
+
+    sleep(5)
