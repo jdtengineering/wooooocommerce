88d2747ab2dd69dd83d253807f70858520f0ccf3
Jim Teunis <jim.teunis@gmail.com>
2024-05-27 14:41:49 +0200

    add option to play special sound when order is over specific amount

--- wooooocommerce.py
+++ wooooocommerce.py
@@ -9,13 +9,20 @@ ds = "2012-03-01T10:00:00Z"  # or any date sting of differing formats.
 date = parser.parse(ds)
 from playsound import playsound
 
+
 SOUND_FILENAME = "sound.wav"
+SPECIAL_SOUND_FILENAME = "special_sound.wav"
 sound = None
+specialsound = None
 
 # sound settings
 # 24 hour notation
 no_sound_before = 11
 no_sound_after = 18
+
+# if the order total (excl. shipping) is greater than this value,
+# play the special sound
+magic_value = 250
 
 # change working directory to this folder
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -28,6 +35,14 @@ try:
 except IOError:
     print("Audio file not found")
     sound = None
+
+# check if special_sound.wav exists
+try:
+    open(SPECIAL_SOUND_FILENAME, "r")
+    specialsound = True
+except IOError:
+    print("Special audio file not found")
+    specialsound = None
 
 # get secret keys from file
 with open("keys.json") as f:
@@ -66,7 +81,10 @@ while True:
                 ):
                     print("new order!!!")
                     if sound and no_sound_before < datetime.now().hour < no_sound_after:
-                        playsound(SOUND_FILENAME)
+                        if float(latest_order['total']) - float(latest_order['shipping_total']) > magic_value and specialsound:
+                            playsound(SPECIAL_SOUND_FILENAME)
+                        else:
+                            playsound(SOUND_FILENAME)
                     # write new last order
                     with open("last_order.txt", "w") as f:
                         f.write(latest_order_str)
